1d9ffa7af72a229e8debf739e20a40417b0e97df
馋猫 <donghai.kang@dataenlighten.com>
2020-04-08 23:57:12 +0800

    reducer初步搭建

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './js/App';
 import WelcomePage from './js/page/WelcomePage';
 import {name as appName} from './app.json';
 import AppNavigator from './js/navigator/AppNavigator';
 
-AppRegistry.registerComponent(appName, () => AppNavigator);
+AppRegistry.registerComponent(appName, () => App);
